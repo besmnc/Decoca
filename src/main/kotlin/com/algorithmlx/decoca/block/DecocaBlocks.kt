e122a1296e7a27b9c8d5075af16e4a62055966d2
algorithmlx <izhukov2007@gmail.com>
2024-03-09 20:28:13 +0700

    Updates

--- src/main/kotlin/com/algorithmlx/decoca/block/DecocaBlocks.kt
+++ src/main/kotlin/com/algorithmlx/decoca/block/DecocaBlocks.kt
@@ -4,8 +4,10 @@ import com.algorithmlx.decoca.ModId
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.*
 import net.minecraft.world.level.block.state.BlockBehaviour
+import net.minecraft.world.level.block.state.BlockBehaviour.Properties
 import net.minecraft.world.level.material.Material
 import net.minecraftforge.registries.DeferredRegister
 import net.minecraftforge.registries.ForgeRegistries
@@ -13,24 +15,52 @@ import thedarkcolour.kotlinforforge.forge.ObjectHolderDelegate
 import thedarkcolour.kotlinforforge.forge.registerObject
 
 object DecocaBlocks {
+    private val blockTab = object : CreativeModeTab("$ModId.blocks") { override fun makeIcon(): ItemStack = ItemStack(carfstone) }
+    private val wallTab = object : CreativeModeTab("$ModId.walls") { override fun makeIcon(): ItemStack = ItemStack(carfstoneWall) }
+    private val stairsTab = object : CreativeModeTab("$ModId.stairs") { override fun makeIcon(): ItemStack = ItemStack(carfstoneStairs) }
+    private val slabTab = object : CreativeModeTab("$ModId.slabs") { override fun makeIcon(): ItemStack = ItemStack(carfstoneSlab) }
+
     val blocks = DeferredRegister.create(ForgeRegistries.BLOCKS, ModId)
     val items = DeferredRegister.create(ForgeRegistries.ITEMS, ModId)
 
-    val carfstone by block("carfstone") { Block(BlockBehaviour.Properties.of(Material.STONE)) }
-    val carfstoneWall by block("carfstone_wall") { WallBlock(BlockBehaviour.Properties.copy(carfstone)) }
-    val carfstoneStairs by block("carfstone_stairs") { StairBlock(carfstone::defaultBlockState, BlockBehaviour.Properties.copy(carfstone)) }
-    val carfstoneSlab by block("carfstone_slab") { SlabBlock(BlockBehaviour.Properties.copy(carfstone)) }
+    val carfstone by block("carfstone") { Block(Properties.of(Material.STONE)) }
+    val carfstoneWall by wall("carfstone_wall") { WallBlock(Properties.copy(carfstone)) }
+    val carfstoneStairs by stair("carfstone_stairs") { StairBlock(carfstone::defaultBlockState, Properties.copy(carfstone)) }
+    val carfstoneSlab by slab("carfstone_slab") { SlabBlock(Properties.copy(carfstone)) }
 
-    val carfstoneBrick by block("carfstone_bricks") { Block(BlockBehaviour.Properties.of(Material.STONE)) }
-    val carfstoneBrickWall by block("carfstone_brick_wall") { WallBlock(BlockBehaviour.Properties.copy(carfstoneBrick)) }
-    val carfstoneBrickStairs by block("carfstone_brick_stairs") { StairBlock(carfstoneBrick::defaultBlockState, BlockBehaviour.Properties.copy(carfstoneBrick)) }
-    val carfstoneBrickSlab by block("carfstone_brick_slab") { SlabBlock(BlockBehaviour.Properties.copy(carfstoneBrick)) }
+    val smoothCarfstone by block("smooth_carfstone") { Block(Properties.of(Material.STONE)) }
+    val smoothCarfstoneWall by wall("smooth_carfstone_wall") { WallBlock(Properties.copy(smoothCarfstone)) }
+    val smoothCarfstoneStairs by stair("smooth_carfstone_stairs") { StairBlock(smoothCarfstone::defaultBlockState, Properties.copy(smoothCarfstone)) }
+    val smoothCarfstoneSlab by slab("smooth_carfstone_slab") { SlabBlock(Properties.copy(smoothCarfstone)) }
 
-    fun <T: Block> block(id: String, block: ()-> T): ObjectHolderDelegate<T> =
-        block(id, block, Item.Properties().tab(CreativeModeTab.TAB_MATERIALS))
+    val carfstoneBrick by block("carfstone_bricks") { Block(Properties.of(Material.STONE)) }
+    val carfstoneBrickWall by wall("carfstone_brick_wall") { WallBlock(Properties.copy(carfstoneBrick)) }
+    val carfstoneBrickStairs by stair("carfstone_brick_stairs") { StairBlock(carfstoneBrick::defaultBlockState, Properties.copy(carfstoneBrick)) }
+    val carfstoneBrickSlab by slab("carfstone_brick_slab") { SlabBlock(Properties.copy(carfstoneBrick)) }
 
-    fun <T: Block> block(id: String, block: () -> T, props: Item.Properties): ObjectHolderDelegate<T> {
-        val regBlock =  blocks.registerObject(id, block)
+    private fun <T: Block> block(id: String, block: ()-> T): ObjectHolderDelegate<T> =
+        block(id, block, Item.Properties().tab(blockTab))
+
+    private fun <T: WallBlock> wall(id: String, block: ()-> T): ObjectHolderDelegate<T> =
+        block(id, block, Item.Properties().tab(wallTab))
+
+    private fun <T: StairBlock> stair(id: String, block: ()-> T): ObjectHolderDelegate<T> =
+        block(id, block, Item.Properties().tab(stairsTab))
+
+    private fun <T: SlabBlock> slab(id: String, block: ()-> T): ObjectHolderDelegate<T> =
+        block(id, block, Item.Properties().tab(slabTab))
+
+    private fun wall(id: String, copy: Block): ObjectHolderDelegate<WallBlock> =
+        wall(id) { WallBlock(Properties.copy(copy)) }
+
+    private fun stair(id: String, copy: Block): ObjectHolderDelegate<StairBlock> =
+        stair(id) { StairBlock(copy::defaultBlockState, Properties.copy(copy)) }
+
+    private fun slab(id: String, copy: Block): ObjectHolderDelegate<SlabBlock> =
+        slab(id) { SlabBlock(Properties.copy(copy)) }
+
+    private fun <T: Block> block(id: String, block: () -> T, props: Item.Properties): ObjectHolderDelegate<T> {
+        val regBlock = blocks.registerObject(id, block)
         items.registerObject(id) { BlockItem(regBlock.invoke(), props) }
         return regBlock
     }
